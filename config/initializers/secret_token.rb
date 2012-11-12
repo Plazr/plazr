0d53ea66a6b77c1d7fcd9a02574be1bce516291e
ricardo-santos <tbag.115@gmail.com>
2012-11-12 08:23:53 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plazr::Application.config.secret_token = '982828a196898f8482c747fb37338a7e9dc20950f62a4537954e991180075141c6c38c4bc9d0fc8e7f3490b7bdb7eaac4fbf620402719a686e938f6add6aae5c'
