101b0a82f468ea0afb654f1eab6a8157c6e998ca
ricardo-santos <tbag.115@gmail.com>
2012-11-14 11:01:22 +0000

    migrations added

---
+++ db/migrate/20121112214714_create_stores.rb
@@ -0,0 +1,13 @@
+class CreateStore < ActiveRecord::Migration
+  def change
+    create_table :stores do |t|
+      t.string :name
+      t.string :description
+      t.string :email
+      t.integer :phone
+      t.string :url
+      
+      t.timestamps
+    end
+  end
+end
