8bc04117c62a1639dbbc55a3e7e9bdc0bc795d16
Miguel Palhas <mpalhas@gmail.com>
2013-01-22 22:38:43 +0000

    fixed  missing

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -53,6 +53,7 @@ class StoresController < ApplicationController
 	
 	  unless current_user.roles.detect { |role| role.name == 'admin' }
       	current_user.roles << PlazrAuth::Role.find_by_name('admin')
+      end
 
       format.html { redirect_to store_path (@store), :notice => 'Store was successfully created.' }
 
